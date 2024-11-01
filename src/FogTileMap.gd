0d4e5c5e90859a92e25d2cdbd655fc5b8b50d4e7
shunlog <artiombalan331@gmail.com>
2024-11-01 22:12:21 +0200

    add fog of war, various improvements

---
+++ src/FogTileMap.gd
@@ -0,0 +1,28 @@
+extends TileMap
+
+#var p: KinematicBody2D  # player
+onready var p := get_tree().get_root().get_node("Node2D/Player")
+
+func _ready():
+	# it's useful to have it set to invisible in the editor
+	visible = true
+
+
+func update_fog():
+	# update the cells around the player
+	var r = 30
+	var pos = p.position / 16
+	for dx in range(-r, r):
+		for dy in range(-r, r):
+			var dist = sqrt(dx * dx + dy * dy)
+			if dist > r:
+				continue
+			var np = pos + Vector2(dx, dy)
+			set_cellv(np, 4)
+	
+	
+	
+	
+	
+	
+	
