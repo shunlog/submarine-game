7335df2b17afd3d3d36013f7a58c25f69d6506f4
shunlog <artiombalan331@gmail.com>
2024-12-12 14:15:23 +0200

    improve friction and acceleration

--- src/KinematicBody2D.gd
+++ src/KinematicBody2D.gd
@@ -2,12 +2,13 @@ extends KinematicBody2D
 
 signal break_tile(posn, damage)
 
-var max_speed = 300  # px/s
-var acceleration = 20  # px/s gained every second
-var friction = 8  # px/s lost every second in water
+var max_speed = 200  # px/s
+var accel_fraction = 0.07  # fraction of the max_speed gained every frame on move
+var friction = .05  # fraction of velocity lost every frame in water
 var gravity_accel = 15  # px/s gained every second outside water
 
-var velocity = Vector2.ZERO  # Player's velocity
+var velocity := Vector2.ZERO  # Player's velocity
+onready var sprite : Sprite = $Sprite
 
 
 func _input(event):
@@ -18,7 +19,8 @@ func _input(event):
 	
 	
 func _physics_process(delta):
-	var accel := Vector2.ZERO
+	var move_accel = max_speed * accel_fraction  # px/s gained every second
+	var force := Vector2.ZERO
 	
 	var dirn := Vector2.ZERO
 	if Input.is_action_pressed("right"):
@@ -29,16 +31,22 @@ func _physics_process(delta):
 		dirn.y += 1
 	if Input.is_action_pressed("up"):
 		dirn.y -= 1
-	accel += dirn.normalized() * acceleration
+	force += dirn.normalized() * move_accel
+	if dirn.x == -1:
+		sprite.flip_h = true
+	elif dirn.x == 1:
+		sprite.flip_h = false
 	
 	var underwater := position.y > 0
 	if underwater:
-		velocity += accel
-		velocity = velocity.move_toward(Vector2.ZERO, friction)
+		velocity += force
+		velocity -= velocity * friction
+		# velocity = velocity.move_toward(Vector2.ZERO, friction)
 	else:
 		velocity += Vector2(0, gravity_accel)
 	
 	velocity = velocity.clamped(max_speed)
+	print(velocity.length())
 
 	# Move the player with KinematicBody2D's move_and_slide method
 	velocity = move_and_slide(velocity)
