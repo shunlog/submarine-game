e14e28f6478be72e5651bc711c252544979a1801
shunlog <artiombalan331@gmail.com>
2024-12-13 20:02:22 +0200

    pause menu

--- src/GUICanvasLayer.gd
+++ src/GUICanvasLayer.gd
@@ -2,12 +2,19 @@ extends CanvasLayer
 
 var showing := false
 
+func _ready():
+	toggle(false)
+
 func _input(event):
 	if event.is_action_pressed("pause"):
 		toggle()
 	
-func toggle():
-	showing = !showing
+func toggle(val=null):
+	if val == null:
+		showing = !showing
+	else:
+		showing = val
+	
 	if showing:
 		get_tree().paused = true
 		show()
