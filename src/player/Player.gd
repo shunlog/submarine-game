d9255ec91a7dba4e4b328da7af85fa4e0e87da5c
shunlog <artiombalan331@gmail.com>
2025-01-19 20:54:45 +0200

    add dashboard

--- src/player/Player.gd
+++ src/player/Player.gd
@@ -2,30 +2,39 @@ extends KinematicBody2D
 
 signal break_tile(posn, damage)
 
-var max_speed = 2000  # px/s
-var accel_fraction = 0.07  # fraction of the max_speed gained every frame on move
-var friction = .05  # fraction of velocity lost every frame in water
-var gravity_accel = 15  # px/s gained every second outside water
-
-var velocity := Vector2.ZERO  # Player's velocity
-onready var sprite : Sprite = $Sprite
 
 export var sonar_color : Color
+const initial_max_speed = 400 # px/s
+const accel_fraction = 0.07  # fraction of the max_speed gained every frame on move
+const friction = .05  # fraction of velocity lost every frame in water
+const gravity_accel = 15  # px/s gained every second outside water
 
+
+var max_speed = initial_max_speed  # px/s
+var velocity := Vector2.ZERO  # Player's velocity
 var sonar := false
+
+
+onready var sprite := $Sprite
+onready var NodeShadow := $ShadowLight
+onready var LabelSpeed := $GUICanvasLayer/MarginContainer/VBoxContainer/debug/HBoxContainer/LabelSpeed
+onready var LabelSonar := $GUICanvasLayer/MarginContainer/VBoxContainer/debug/HBoxContainer2/LabelSonar
+
+
 func toggle_sonar(v: bool = !sonar):
 	sonar = v
-	if v:
-		$ShadowLight.color = sonar_color
-		$ShadowLight.shadow_enabled = false
-	else:
-		$ShadowLight.color = Color.white
-		$ShadowLight.shadow_enabled = true
+	LabelSonar.text = str(v)
+	NodeShadow.shadow_enabled = !v
+	NodeShadow.color = sonar_color if v else Color.white
 
 
+## v is between 0 and 4
 func set_speed(v: float):
-	print(v)
+	max_speed = initial_max_speed * (1 + 0.2 * v)
 
+
+func _ready():
+	toggle_sonar(false)
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -34,6 +43,8 @@ func _input(event):
 	elif event.is_action_pressed("sonar"):
 		toggle_sonar()
 
+func _process(delta):
+	_update_labels()
 
 func _physics_process(delta):
 	var move_accel = max_speed * accel_fraction  # px/s gained every second
@@ -72,3 +83,6 @@ func _on_Area2DRadar_area_entered(area: Area2D):
 	# Assuming the Area2D is a child of the enemy with a "target" method
 	var actor = area.get_parent()
 	actor.target(self)
+
+func _update_labels():
+	LabelSpeed.text = '%8.2f' % [velocity.length()]
